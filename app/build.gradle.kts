7ca67d18f4fe2f4135da9f87f40ff8132344d50f
MohRjoub <mohammadrojoob@gmail.com>
2025-05-17 09:00:53 +0300

    Add Login

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.android.volley)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
--- app/src/main/java/com/example/project_school/MainActivity.java
+++ app/src/main/java/com/example/project_school/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.project_school;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,9 +13,24 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity {
 
     private Button login;
+    private EditText username;
+    private EditText password;
+    private RequestQueue queue;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,12 +38,48 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         login = findViewById(R.id.loginButton);
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+
+        queue = Volley.newRequestQueue(this);
+
 
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, HomePage.class);
-                startActivity(intent);
+                String user = username.getText().toString();
+                String pass = password.getText().toString();
+                String url = "http://10.0.2.2:80/project_school/getUser.php?user_id=" + user;
+                JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url,
+                        null,
+                        new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                try {
+                                    boolean isError = response.getBoolean("error");
+                                    if (!isError) {
+                                        JSONObject user = response.getJSONObject("user");
+                                        String pass_db = user.getString("password");
+                                        if (pass.equals(pass_db)) {
+                                            String name = user.getString("name");
+                                            Intent intent = new Intent(MainActivity.this, HomePage.class);
+                                            intent.putExtra("name", name);
+                                            startActivity(intent);
+                                        }
+                                    }
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                Log.d("Error", error.toString());
+                            }
+                        }
+                );
+                queue.add(request);
             }
         });
 
