3332ef9b52adbaacd04f3e66c0d2432e19006fab
walat <waleedlaith77@gmail.com>
2025-05-23 19:40:02 +0300

    laith update

--- app/src/main/java/com/example/project_school/HomePage.java
+++ app/src/main/java/com/example/project_school/HomePage.java
@@ -2,6 +2,8 @@ package com.example.project_school;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +12,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class HomePage extends AppCompatActivity {
+
+    private Button myCoursesBtn,myAssignmentsBtn,myMarksBtn,mySchduleBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +28,67 @@ public class HomePage extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        setupViews();
+        setScheduleBtn();
+        setMarksBtn();
+        setAssignmentsBtn();
+        setCoursesBtn();
+
     }
+
+    private void setupViews(){
+        mySchduleBtn=findViewById(R.id.mySchduleBtn);
+        myMarksBtn=findViewById(R.id.myMarksBtn);
+        myAssignmentsBtn=findViewById(R.id.myAssignmentsBtn);
+        myCoursesBtn=findViewById(R.id.myCoursesBtn);
+    }
+
+    private void setScheduleBtn(){
+        mySchduleBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomePage.this,studentSchedule.class);
+               // intent.putExtra("NAME",userName);
+               
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void setMarksBtn(){
+        mySchduleBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomePage.this,studentSchedule.class);
+                // intent.putExtra("NAME",userName);
+
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void setAssignmentsBtn(){
+        mySchduleBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomePage.this,studentSchedule.class);
+                // intent.putExtra("NAME",userName);
+
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void setCoursesBtn(){
+        mySchduleBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomePage.this,studentSchedule.class);
+                // intent.putExtra("NAME",userName);
+
+                startActivity(intent);
+            }
+        });
+    }
+    
 }
